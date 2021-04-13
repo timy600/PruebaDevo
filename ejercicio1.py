59fc55d7938bb4f8647625a0d4876588eac0e86e
Thibaut chevée <tchevee@hotmail.fr>
2021-04-13 15:05:04 +0200

    ejercicio1. respuesta con bucles basicos.

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,11 +4,25 @@ si es un número perfecto, abundante o defectivo.
 
 
 def verificar_lista_numeros_perfectos(lista):
+    lista_respuesta = []
     # Verificar que el parametro sea una lista
     # Boucle sobre la lista de parametros.
+    for n in lista:
         # Crear una lista de divisores, hasta n/2.
+        divisores = []
         # Boucle sobre la lista de divisores.
-            # Comparar la suma con el numero.
-                # Añadir el resultado a la lista_respuesta. 
+        x = 1
+        while x <= (n/2):
+            if n % x == 0:
+                divisores.append(x)
+            x = x + 1
+        # Comparar la suma con el numero.
+        # Añadir el resultado a la lista_respuesta. 
+        if sum(divisores) < n:
+            lista_respuesta.append("defectivo")
+        elif sum(divisores) == n:
+            lista_respuesta.append("perfecto")
+        elif sum(divisores) > n:
+            lista_respuesta.append("abundante")
 
     return lista_respuesta
